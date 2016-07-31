df9ec2743268d35547e9d3a234fea98e56dcb67d
Zach Stone <hexaphixylix@yahoo.com>
2016-07-31 15:10:44 -0400

    remove old render method

--- src/js/audio-block.js
+++ src/js/audio-block.js
@@ -30,20 +30,6 @@ class AudioBlock {
             y: this.position.y + 30}],
     ]);
   }
-
-  old_render() {
-    return [
-      '<rect class="audio-block" ',
-      'x="', this.position.x, '"',
-      'y="', this.position.y, '"',
-      'width="128" height="128">',
-      '</rect>',
-      '<text x="', this.position.x + 10, '"',
-      'y="', this.position.y + 25, '" fill="#eee">', this.displayName, '</text>',
-      '<rect class="audio-port" x="', this.position.x - 5, '"',
-      'y="', this.position.y + 30, '"></rect>',
-    ].reduce((pre, cur) => pre + cur);
-  }
 }
 
 module.exports.AudioBlock = AudioBlock;
